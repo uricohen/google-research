da5a323d698b8fb8295572b64a03878cc20cd83f
uric <uri.cohen@gmail.com>
2019-07-25 11:14:33 +0300

    Parsing training and evaluation set results

--- demogen/parse_results.py
+++ demogen/parse_results.py
@@ -3,6 +3,7 @@ import demogen.data_util as data_util
 import demogen.model_config as mc
 import json
 import os
+import numpy as np
 import scipy.io
 
 root_dir = '../concept/data/demogen_models/'
@@ -53,6 +54,34 @@ for i in range(len(model_types)):
                                             copy=copy,
                                             root_dir=root_dir)
                   count+=1
+                  train_loss=np.NaN
+                  train_cross_entropy=np.NaN
+                  train_global_step=np.NaN
+                  train_accuracy=np.NaN
+                  eval_loss=np.NaN
+                  eval_cross_entropy=np.NaN
+                  eval_global_step=np.NaN
+                  eval_accuracy=np.NaN
+                  try:
+                    # Training metadata
+                    results = os.path.join(model_config.get_model_dir_name(),'train.json')
+                    with tf._api.v1.io.gfile.GFile(results, 'r') as f:
+                      dd = json.load(f)
+                    train_loss = dd['loss']
+                    train_cross_entropy = dd['CrossEntropy']
+                    train_global_step = dd['global_step']
+                    train_accuracy = dd['Accuracy']
+                    results = os.path.join(model_config.get_model_dir_name(),'eval.json')
+                    # Validation metadata
+                    with tf._api.v1.io.gfile.GFile(results, 'r') as f:
+                      dd = json.load(f)
+                    eval_loss = dd['loss']
+                    eval_cross_entropy = dd['CrossEntropy']
+                    eval_global_step = dd['global_step']
+                    eval_accuracy = dd['Accuracy']
+                  except tf.errors.NotFoundError:
+                    pass
+                  # Other information
                   filename = os.path.basename(model_config.get_model_dir_name())
                   print('%d %s' % (count, filename))      
                   fd = {'model_type':model_type, 'dataset':dataset, 
@@ -60,6 +89,10 @@ for i in range(len(model_types)):
                         'dropout_prob':dropout_prob, 'data_augmentation':augmentation, 
                         'l2_decay_factor':decay_fac, 'normalization':normalization, 
                         'learning_rate':learning_rate, 'copy':copy,
+                        'eval_loss':eval_loss, 'eval_cross_entropy':eval_cross_entropy,
+                        'eval_global_step':eval_global_step, 'eval_accuracy':eval_accuracy,
+                        'train_loss':train_loss, 'train_cross_entropy':train_cross_entropy,
+                        'train_global_step':train_global_step, 'train_accuracy':train_accuracy,
                         'filename':filename}
                   scipy.io.savemat('%s_%s_%s.mat'%(model_type,dataset,filename), fd);
                   #input_fn = data_util.get_input(data_dir, data=model_config.dataset, data_format=model_config.data_format)
